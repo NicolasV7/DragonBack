f042075fcf1636b166679e074bc0345166e6d51f
Nicolas <nicolas@dlpbasculas.com>
2024-12-01 22:50:21 -0500

    configure database connection using environment variables; add dotenv dependency

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('railway', 'postgres', 'JJqRYQFBeTkaVEpUzWNdsWamWFutJPJD', {
-  host: 'junction.proxy.rlwy.net',
-  port: 57274,
-  dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
 });
 
 module.exports = sequelize;
